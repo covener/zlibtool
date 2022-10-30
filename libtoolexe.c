a37b054f66b67291ed4fb8b9dad3f347291f3657
Eric Covener <ecovener@us.ibm.com>
2022-10-30 18:35:08 -0400

    bump to 1.4.0 for IHS 2.4

--- libtoolexe.c
+++ libtoolexe.c
@@ -1995,7 +1995,7 @@ static int version(Parms_t *p)
    *       number out of the output.
    */
 
-  printf(PGM ": This is libtool 1.3.9 for " PLATFORM " %s\n"
+  printf(PGM ": This is libtool 1.4.0 for " PLATFORM " %s\n"
          "It acts enough like GNU libtool to allow Apache to be built.\n", rcsid);
   return 0;
 }
